e6b452543fffef07accf8cc4ebd630de79ac456f
Jacky Lui <jackymlui@live.com>
2018-04-27 20:42:17 +1000

    finished react basic

--- pro-mern-stack/static/App.js
+++ pro-mern-stack/static/App.js
@@ -8,7 +8,40 @@ const component = React.createElement(
     message,
     React.createElement('br', null)
 );
+
+const issues = [{
+    id: 1, status: 'Open', owner: 'Raven',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add'
+}, {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14,
+    completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel'
+}];
+
 class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            issues: []
+        };
+        this.createIssue = this.createIssue.bind(this);
+    }
+    componentDidMount() {
+        this.loadData();
+    }
+    loadData() {
+        setTimeout(() => {
+            this.setState({ issues: issues });
+        }, 500);
+    }
+    createIssue(newIssue) {
+        const newIssues = this.state.issues.slice();
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({ issues: newIssues });
+    }
     render() {
         return React.createElement(
             'div',
@@ -19,8 +52,8 @@ class IssueList extends React.Component {
                 'IssueTracker'
             ),
             React.createElement(IssueFilter, null),
-            React.createElement(IssueTable, null),
-            React.createElement(IssueAdd, null)
+            React.createElement(IssueTable, { issues: this.state.issues }),
+            React.createElement(IssueAdd, { createIssue: this.createIssue })
         );
     }
 }
@@ -35,79 +68,140 @@ class IssueFilter extends React.Component {
     }
 }
 
-class IssueTable extends React.Component {
-    render() {
-        const borderedStyle = { border: "1px solid red", padding: 6 };
-        return React.createElement(
-            'table',
-            { style: { borderCollapse: "collapse" } },
-            React.createElement(
-                'thead',
-                null,
-                React.createElement(
-                    'tr',
-                    null,
-                    React.createElement(
-                        'th',
-                        { style: borderedStyle },
-                        'Id'
-                    ),
-                    React.createElement(
-                        'th',
-                        { style: borderedStyle },
-                        'Title'
-                    )
-                )
-            ),
-            React.createElement(
-                'tbody',
-                null,
-                React.createElement(
-                    IssueRow,
-                    { issue_id: 1 },
-                    'Error in console when clicking Add'
-                ),
-                React.createElement(
-                    IssueRow,
-                    { issue_id: 2 },
-                    'Missing bottom border on panel'
-                )
-            )
-        );
-    }
-}
-
-class IssueRow extends React.Component {
-    render() {
-        const borderedStyle = { border: "1px solid silver", padding: 4 };
-        return React.createElement(
-            'tr',
+function IssueTable(props) {
+    // when it's not a single expression
+    const issueRows = props.issues.map(issue => React.createElement(IssueRow, { key: issue.id, issue: issue }));
+    return React.createElement(
+        'table',
+        { className: 'bordered-table' },
+        React.createElement(
+            'thead',
             null,
             React.createElement(
-                'td',
-                { style: borderedStyle },
-                this.props.issue_id
-            ),
-            React.createElement(
-                'td',
-                { style: borderedStyle },
-                this.props.children
+                'tr',
+                null,
+                React.createElement(
+                    'th',
+                    null,
+                    'Id'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Status'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Owner'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Created'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Effort'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Completion'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Title'
+                )
             )
-        );
-    }
+        ),
+        React.createElement(
+            'tbody',
+            null,
+            issueRows
+        )
+    );
 }
 
-IssueRow.propTypes = {
-    issue_id: React.PropTypes.number.isRequired,
-    issue_title: React.PropTypes.string
-};
+const IssueRow = props => // 2015 arrow function
+React.createElement(
+    'tr',
+    null,
+    React.createElement(
+        'td',
+        null,
+        props.issue.id
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.status
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.owner
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.created.toDateString()
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.effort
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.completionDate ? props.issue.completionDate.toDateString() : ""
+    ),
+    React.createElement(
+        'td',
+        null,
+        props.issue.title
+    )
+);
+
+//   IssueRow.propTypes = {
+//       issue_id:React.PropTypes.number.isRequired,
+//       issue_title:React.PropTypes.string
+//   };
 
 class IssueAdd extends React.Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        var form = document.forms.issueAdd;
+        this.props.createIssue({
+            owner: form.owner.value, // not captured in React state, which could have been updated in server as a backup.
+            title: form.title.value, // only in browser
+            status: "New",
+            created: new Date()
+        });
+        form.owner.value = "";form.title.value = "";
+    }
     render() {
         return React.createElement(
             'div',
             null,
-            ' This is a placeholder for an Issue Add entry form'
+            React.createElement(
+                'form',
+                { name: 'issueAdd', onSubmit: this.handleSubmit },
+                React.createElement('input', { type: 'text', name: 'owner', placeholder: 'Owner' }),
+                React.createElement('input', { type: 'text', name: 'title', placeholder: 'Title' }),
+                React.createElement(
+                    'button',
+                    null,
+                    'Add'
+                )
+            )
         );
     }
 }
