22429f3e411a9ac428f1ebd02a433cf21b88753f
Jacky Lui <jackymlui@live.com>
2018-05-22 08:24:41 +1000

    posting to mongodb

--- pro-mern-stack/server.js
+++ pro-mern-stack/server.js
@@ -32,7 +32,7 @@ const validIssueStatus = {
 };
 
 const issueFieldType={
-    id:"required",
+    // id:"required",
     status:"required",
     owner:"required",
     effort:"optional",
@@ -52,19 +52,19 @@ function validateIssue(issue){
     }
 }
 
-const issues =[
-    {
-        id: 1, status:'Open', owner:'Raven',
-        created:new Date('2016-08-15'), effort: 5,completionDate:undefined,
-        title:'Error in console when clicking Add',
-    },
-    {
-        id:2, status:'Assigned', owner:'Eddie', 
-        created: new Date('2016-08-16'), effort: 14,
-        completionDate:new Date('2016-08-30'), 
-        title:'Missing bottom border on panel',
-    },
-];
+// const issues =[
+//     {
+//         id: 1, status:'Open', owner:'Raven',
+//         created:new Date('2016-08-15'), effort: 5,completionDate:undefined,
+//         title:'Error in console when clicking Add',
+//     },
+//     {
+//         id:2, status:'Assigned', owner:'Eddie', 
+//         created: new Date('2016-08-16'), effort: 14,
+//         completionDate:new Date('2016-08-30'), 
+//         title:'Missing bottom border on panel',
+//     },
+// ];
 
 app.get("/api/issues", (req,res)=>{
     // const metadata = {total_count:issues.length};
@@ -84,7 +84,7 @@ app.post("/api/issues", (req,res)=>{
     console.log("posted!");
     const newIssue = req.body;
     console.log(newIssue);
-    newIssue.id = issues.length+1;
+    // newIssue.id = issues.length+1;
     newIssue.created = new Date();
     if(!newIssue.status) newIssue.status = "New";
     const err = validateIssue(newIssue);
@@ -92,8 +92,15 @@ app.post("/api/issues", (req,res)=>{
         res.status(422).json({message:"Invalid request:" + err});
         return;
     }
-    issues.push(newIssue);
-    res.json(newIssue);
+    db.collection('issues').insertOne(newIssue).then(result =>
+        db.collection('issues').find({_id:result.insertedId}).limit(1).next
+    ).then(newIssue =>{
+        res.json(newIssue);
+    }).catch(error=>{
+        console.log(error);
+        res.status(500).json({message:'Internal Server Error: ${error}'});
+    })
+    // issues.push(newIssue);
 });
 
 
